169592ccfa7979555da89c294b1bfe8091879426
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-11-21 07:06:50 -0600

    hello world!

---
+++ spring.demo/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach {
+	public String getDailyWorkout() {
+		return "spend 30 minutes on batting";
+	}
+
+}
---
+++ spring.demo/src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+	//create object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		//use object
+	System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
